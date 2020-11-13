143a9487d0ec87108c70d9336022367b654c36f1
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2020-11-12 23:48:52 -0500

    Minor changes

--- test/escape/EscapeGameBuilderTest.java
+++ test/escape/EscapeGameBuilderTest.java
@@ -12,7 +12,13 @@
 
 package escape;
 
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import escape.required.*;
+import escape.required.EscapePiece.PieceName;
 
 /**
  * This is a simple test, not really a unit test, to make sure tht the
@@ -22,9 +28,28 @@ import org.junit.jupiter.api.Test;
  */
 class EscapeGameBuilderTest {
 
+	private static EscapeGameManager manager;
+
+	@BeforeAll
+	static void loadGame() throws Exception
+	{
+		EscapeGameBuilder egb = new EscapeGameBuilder("config/egc/GameBuilderTest.egc");
+		manager = egb.makeGameManager();
+		assertNotNull(manager);
+	}
+	
 	@Test
 	void test() throws Exception {
 		EscapeGameBuilder egb = new EscapeGameBuilder("config/egc/test1.egc");
+	}
+
+	@Test
+	void horseAt6_2()
+	{
+		EscapePiece piece = manager.getPieceAt(manager.makeCoordinate(6, 2));
+		assertNotNull(piece);
+		assertEquals(PieceName.HORSE, piece.getName());
+		assertEquals(Player.PLAYER1, piece.getPlayer());
 	}
 
 }
